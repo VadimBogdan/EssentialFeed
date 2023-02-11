f02dc6c46cb19179169956145000db6930a5bdca
Vadym Bohdan <vadymbog@gmail.com>
2023-02-11 14:45:03 +0200

    create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vadym Bohdan on 10.02.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
