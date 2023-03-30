2f0de2e0448b82e633a2898e05cdd60f8265d7f9
Vadym Bohdan <vadymbog@gmail.com>
2023-03-30 11:30:29 +0300

    Dispatch `CodableFeedStore` operations in a serial background queue to avoid blocking clients. And because queue runs in background it can run `FileManager.default.removeItem` slower so I increased expectation timeout to 20.0 seconds

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -181,7 +181,7 @@ final class CodableFeedStoreTests: XCTestCase {
             deletionError = receivedDeletionError
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 20.0)
         return deletionError
     }
     
