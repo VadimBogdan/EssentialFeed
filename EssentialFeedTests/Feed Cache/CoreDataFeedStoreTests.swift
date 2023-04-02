1a92b0b0d48931e7105b373bed91231daf3d132a
Vadym Bohdan <vadymbog@gmail.com>
2023-04-02 18:51:53 +0300

    Load persistent container stores upon `CoreDataFeedStore` initialization

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -12,14 +12,14 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
-
-         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+        
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-
-         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+        
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
@@ -63,11 +63,12 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     // - MARK: Helpers
-     
-     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-         let sut = CoreDataFeedStore()
-         trackForMemoryLeaks(sut, file: file, line: line)
-         return sut
-     }
-
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let sut = try! CoreDataFeedStore(bundle: storeBundle)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
 }
