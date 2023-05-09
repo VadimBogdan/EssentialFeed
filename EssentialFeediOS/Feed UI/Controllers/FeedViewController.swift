5801e58569c7fbf82bfb9f772ca04c2d11955d22
Vadym Bohdan <vadymbog@gmail.com>
2023-05-09 18:52:08 +0300

    Render error message on load feed error

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -15,7 +15,7 @@ public final class ErrorView: UIView {
     public var message: String?
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
     public let errorView = ErrorView()
     var delegate: FeedViewControllerDelegate?
     
@@ -31,6 +31,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     
     @IBAction private func refresh() {
         delegate?.didRequestFeedRefresh()
+    }
+    
+    func display(_ viewModel: FeedErrorViewModel) {
+        errorView.message = viewModel.message
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
