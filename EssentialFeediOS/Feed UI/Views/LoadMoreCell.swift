26352f355353ab419762638d980544c8dd560b18
Vadym Bohdan <vadymbog@gmail.com>
2023-07-26 16:03:54 +0300

    Remove debug code

--- EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -51,7 +51,6 @@ public class LoadMoreCell: UITableViewCell {
             } else {
                 spinner.stopAnimating()
             }
-            print("isAnimating \(newValue), current: \(spinner.isAnimating)")
         }
     }
     
